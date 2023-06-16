8ab15935d76411fa344f708a2e1c2bd2c6d06394
Skylar A Gering <skygering@gmail.com>
2023-06-16 16:19:00 -0700

    Add remove floe overlap code

--- src/physical_processes/ridge_raft.jl
+++ src/physical_processes/ridge_raft.jl
@@ -10,14 +10,93 @@ function add_floe_volume!(
     vol,
     simp_settings,
     consts,
+    Δt,
 )
+    # Update floe height, mass, and moment of intertia due to volume change
     init_height = floe.height
     floe.height += vol/floe.area
     if floe.height > simp_settings.max_floe_height
         floe.height = simp_settings.max_floe_height
     end
+    mass_tmp = floe.mass
     floe.mass += vol * consts.ρi
+    moment_tmp = floe.moment
     floe.moment *= floe.height/init_height
+    # Update velocities and accelerations to conserve momentum
+    conserve_momentum_combination!(
+        mass_tmp,
+        moment_tmp,
+        floe.centroid[1],
+        floe.centroid[2],
+        Δt,
+        floe,
+    )
+end
+
+function remove_floe_overlap!(
+    keepfloe::Union{Floe{FT}, LazyRow{Floe{FT}}},
+    floediff,
+    vol,
+    coupling_settings,
+    consts,
+    rng,  
+) where {FT <: AbstractFloat}
+    new_floe_poly = LG.difference(
+        LG.Polygon(keepfloe.coords),
+        LG.Polygon(floediff.coords),
+    )
+    new_floe_area = LG.area(new_floe_poly)
+    regions = get_polygons(new_floe_poly)
+    new_floes = StructVector{Floe}(undef, length(regions) - 1)
+    for i in eachindex(regions)
+        new_coords = find_poly_coords(regions[i])::PolyVec{FT}
+        new_poly = LG.Polygon(rmholes(new_coords))
+        new_area = LG.area(new_poly)
+        new_mass = new_area/new_floe_area * (keepfloe.mass - vol * consts.ρi)
+        mass_tmp = keepfloe.mass
+        moment_tmp = keepfloe.moment
+        x_tmp, y_tmp = keepfloe.centroid
+        if i == 1  # Update existing floe
+            # Update floe based on new shape
+            replace_floe!(
+                keepfloe,
+                new_poly,
+                new_mass,
+                consts,
+                coupling_settings.mc_n,
+                rng,
+            )
+            # Update velocities and accelerations to conserve momentum
+            conserve_momentum_combination!(
+                mass_tmp,
+                moment_tmp,
+                x_tmp,
+                y_tmp,
+                Δt,
+                keep_floe,
+            )
+        else  # Create new floes
+            new_floes[i] = deepcopy(keepfloe)
+            # Update floe based on new shape
+            replace_floe!(
+                LazyRow(new_floes, i),
+                new_poly,
+                new_mass,
+                consts,
+                coupling_settings.mc_n,
+                rng,
+            )
+            # Update velocities and accelerations to conserve momentum
+            conserve_momentum_combination!(
+                mass_tmp,
+                moment_tmp,
+                x_tmp,
+                y_tmp,
+                Δt,
+                LazyRow(new_floes, i),
+            )
+        end
+    end
 end
 
 function floe_floe_ridge!(
@@ -27,6 +106,7 @@ function floe_floe_ridge!(
     ridgeraft_settings,
     simp_settings,
     consts,
+    Δt,
 )
     vol1 = overlap_area * floe1.height
     vol2 = overlap_area * floe2.height
@@ -35,36 +115,68 @@ function floe_floe_ridge!(
         if rand(1) >= 1/(1 + (floe1.height/floe2.height))
             add_floe_volume!(
                 floe1,
-                overlap_area * floe2.height,
+                vol2,
                 simp_settings,
                 consts,
+                Δt,
             )
-             # ridge value switch 
+            remove_floe_overlap!(
+                floe2,
+                floe1,
+                vol2,
+                coupling_settings,
+                consts,
+                rng,  
+            ) 
         else
             add_floe_volume!(
                 floe2,
-                overlap_area * floe1.height,
+                vol1,
                 simp_settings,
                 consts,
+                Δt,
             )
-             # ridge value switch 
+            remove_floe_overlap!(
+                floe1,
+                floe2,
+                vol1,
+                coupling_settings,
+                consts,
+                rng,  
+            )  
         end
     elseif floe1.height >= ridgeraft_settings.hc && floe2.height < ridgeraft_settings.hc
         add_floe_volume!(
             floe1,
-            overlap_area * floe2.height,
+            vol2,
             simp_settings,
             consts,
+            Δt,
         )
-         # ridge value switch 
+        remove_floe_overlap!(
+            floe2,
+            floe1,
+            vol2,
+            coupling_settings,
+            consts,
+            rng,  
+        ) 
     elseif floe1.height < ridgeraft_settings.hc && floe2.height >= ridgeraft_settings.hc
         add_floe_volume!(
             floe2,
-            overlap_area * floe1.height,
+            vol1,
             simp_settings,
             consts,
+            Δt,
         )
-         # ridge value switch 
+        remove_floe_overlap!(
+            floe1,
+            floe2,
+            vol1,
+            coupling_settings,
+            consts,
+            rng,  
+        ) 
     end
 end
 
