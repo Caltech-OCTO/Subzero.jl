4a2ec5b7b080dd0d7adc5b629e7d389a73e00a2a
Skylar Gering <sgering@g.hmc.edu>
2024-08-07 11:45:15 -0400

    Remove vitepress for simplicity

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,21 @@
-using Documenter, DocumenterVitepress
+using Documenter
 
 using Subzero
 
+format = Documenter.HTML(;
+    repolink = "https://github.com/Caltech-OCTO/Subzero.jl",
+    canonical = "https://Caltech-OCTO.github.io/SubzeroDocumentation/stable",
+    mathengine = MathJax3(),
+    size_threshold = 5*10^5,  # 500 KiB
+)
+
 makedocs(;
     modules=[Subzero],
-    authors="Skylar Gering",
-    repo="https://github.com/Caltech-OCTO/Subzero.jl",
+    authors="Skylar Gering and contributers",
     sitename="Subzero.jl",
-    format=DocumenterVitepress.MarkdownVitepress(
-        repo = "https://github.com/Caltech-OCTO/Subzero.jl",
-        devurl = "dev",
-        deploy_url = "Caltech-OCTO.github.io/Subzero.jl",
-    ),
+    format,
     pages=[
+        "Home" => "index.md",
         "Introduction" => "introduction.md",
         "API Reference" => "api.md",
     ],
